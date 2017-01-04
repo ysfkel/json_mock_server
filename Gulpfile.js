549446d5581f34dc3d6e3ba67cc1f4cc314b53a1
Yusuf <ykelo@ip-10-41-1-196.eu-west-1.compute.internal>
2017-01-04 11:14:42 +0400

    removed unused modules

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,21 +1,5 @@
 var gulp = require('gulp');
 var merge = require('gulp-merge-json');
-var cust = require('./customer.js')
-var run = require('gulp-run');
-var shell = require('gulp-shell')
-
-
-var exec = require('child_process').exec;
-
-
-
-gulp.task('run-num', function() {
-
-    var cmd = new run.Command('node customer.js>db.json'); // create a command object for `cat`. 
-
-    cmd.exec('start');
-})
-
 
 
 
